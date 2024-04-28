ec70f4e5f9847f136981054af8c16016cdc60850
Alexander Bal <alexander.bal@student.hogent.be>
2024-04-28 11:39:14 +0200

    init

---
+++ src/main/java/com/example/paris20224/Paris20224Application.java
@@ -0,0 +1,13 @@
+package com.example.paris20224;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paris20224Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Paris20224Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paris20224/Paris20224ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paris20224;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paris20224ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
