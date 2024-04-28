97d19f3b99f3c25e2f491627e3853a8a1e61de82
Alexander Bal <alexander.bal@student.hogent.be>
2024-04-28 11:55:30 +0200

    Created Homecontroller

---
+++ src/main/java/com/example/paris20224/Homecontroller.java
@@ -0,0 +1,13 @@
+package com.example.paris20224;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Homecontroller {
+
+    @GetMapping("/")
+    public String showHomePage() {
+        return "redirect:/sport/overzicht";
+    }
+}
