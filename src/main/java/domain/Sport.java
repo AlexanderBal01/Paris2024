7fc9462b2a5a9787e36df3ba8a6a1512a7486eda
Alexander Bal <alexander.bal@student.hogent.be>
2024-04-28 12:07:53 +0200

    created Sport class

---
+++ src/main/java/domain/Sport.java
@@ -0,0 +1,15 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Sport implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String name;
+}
