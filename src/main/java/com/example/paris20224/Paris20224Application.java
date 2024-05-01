f6696b43b41fea4bc20b7ffa0742828e4942bf8b
Alexander Bal <alexander.bal@student.hogent.be>
2024-05-01 15:02:30 +0200

    changed startup file

--- src/main/java/com/example/paris20224/Paris20224Application.java
+++ src/main/java/com/example/paris20224/Paris20224Application.java
@@ -2,12 +2,24 @@ package com.example.paris20224;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import service.SportService;
+import service.SportServiceImpl;
 
 @SpringBootApplication
-public class Paris20224Application {
+public class Paris20224Application implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(Paris20224Application.class, args);
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/sports/overview");
+    }
+
+    @Bean
+    SportService sportService() { return new SportServiceImpl(); }
 }
